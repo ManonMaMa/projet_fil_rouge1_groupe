24f20f9d729ef9ef35f84d26cc3decaf6774d179
Nicolas <gagnantnicolas64@gmail.com>
2026-02-03 16:36:29 +0100

    creer le style.css pour compiler tout css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Connexion from "./pages/Connexion"
-
+//import Sidebar from "./composants/Sidebar"
 
 function App() {
   return (
---
+++ frontend/src/composants/Sidebar.tsx
@@ -0,0 +1,12 @@
+import "./Style.css"
+import "./Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="navbar-page">
+            <h1>Coucou !</h1>
+        </div>
+    )
+}
+
+export default Sidebar
