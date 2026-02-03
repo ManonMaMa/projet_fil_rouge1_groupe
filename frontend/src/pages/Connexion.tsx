9ec9ee86dda4628b6cf8167069b863f31e295bfa
Lelelr <rea.leandre@yahoo.com>
2026-02-03 11:39:59 +0100

    Création page Connexion

---
+++ frontend/src/pages/Connexion.tsx
@@ -0,0 +1,18 @@
+import "./Connexion.css"
+
+function Connexion() {
+  return (
+    <div className="connexion-page">
+        
+      <div className="Partie-gauche">
+        <h1></h1>
+      </div>
+
+      <div className="Partie-droite">
+        <h2></h2>
+      </div>
+    </div>
+  )
+}
+
+export default Connexion
