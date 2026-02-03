0d21ff41cd578bc12eaf7bbd0b7f36532e0ba455
ManonMaMa <infini030303@gmail.com>
2026-02-03 15:03:46 +0100

    page inscription minimale

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Connexion from "./pages/Connexion"
+import Inscription from "./pages/Inscription"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Connexion />} />
+        <Route path="/inscription" element={<Inscription />} />
       </Routes>
     </BrowserRouter>
   )
