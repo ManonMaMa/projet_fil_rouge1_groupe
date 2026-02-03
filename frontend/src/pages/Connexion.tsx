387dc3dcffec9a325f234d9d1a59f29021f0e70a
Lelelr <rea.leandre@yahoo.com>
2026-02-03 12:26:16 +0100

    Ajout formulaire simple connexion

--- frontend/src/pages/Connexion.tsx
+++ frontend/src/pages/Connexion.tsx
@@ -4,17 +4,34 @@ function Connexion() {
   return (
     <div className="connexion-page">
 
-      <div className="Partie-gauche">
-        <div className="Contenu">
-            <h1>Bonjour,</h1>
-            <h1>Content de vous revoir</h1>
+        <div className="Partie-gauche">
+            <div className="Contenu">
+                <h2>Logo</h2>
+
+                <h1>Bonjour,</h1>
+                <h1>Content de vous revoir</h1>
+
+                <input
+                type="email"
+                placeholder="email"
+                className="input-field"
+                />
+
+                <input
+                type="password"
+                placeholder="Mot de passe"
+                className="input-field"
+                />
+
+                <button>Connexion</button>
+
+
+            </div>
         </div>
 
-      </div>
+        <div className="Partie-droite">
 
-      <div className="Partie-droite">
-
-      </div>
+        </div>
     </div>
   )
 }
