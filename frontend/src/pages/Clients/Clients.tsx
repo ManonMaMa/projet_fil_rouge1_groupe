988868dd47080c993816383cba8fcc4ff12d4dcd
ManonMaMa <infini030303@gmail.com>
2026-03-04 09:50:26 +0100

    debut page client

--- frontend/src/pages/Clients/Clients.tsx
+++ frontend/src/pages/Clients/Clients.tsx
@@ -38,13 +38,13 @@ const Clients: React.FC = () => {
             <div className="page-contenu">
                 {/* Header */}
                 <Header
-                    titre="Clients"                             // changer le titre de la page
-                    onglets={Onglets}                           // Onglets à afficher
-                    ongletActif="tous"                          // ID de l'onglet actif par défaut
-                    surChangementOnglet={changementOnglet}      // callback pour changement d'onglet
-                    afficherBasculeFiltre={true}                // affiche le bouton bascule filtre
-                    nouvelElement={NouvelleFacture}             // callback pour créer une nouvelle facture
-                    texteBoutonNouvelElement="Nouveau Client  " // Texte du bouton
+                    titre="Clients"                           // changer le titre de la page
+                    onglets={Onglets}                         // Onglets à afficher
+                    ongletActif="tous"                        // ID de l'onglet actif par défaut
+                    surChangementOnglet={changementOnglet}    // callback pour changement d'onglet
+                    afficherBasculeFiltre={true}              // affiche le bouton bascule filtre
+                    nouvelElement={NouvelleFacture}           // callback pour créer une nouvelle facture
+                    texteBoutonNouvelElement="Nouveau Client" // Texte du bouton
                 />
 
 
@@ -55,27 +55,60 @@ const Clients: React.FC = () => {
                     {/* Section de la première année */}
                     <div className="section-annee">
                         <h2 className="titre-annee">2026</h2>
-                        {/* liste des documents ici */}
-                        <p>Devis 1</p>
-                        <p>Devis 2</p>
-                        <p>Devis 3</p>
-                        <p>Devis 4</p>
-                        <p>Devis 5</p>
-                        <p>Devis 6</p>
-                        <p>Devis 7</p>
+                        <table className="tableau_clients">
+                            <thead>
+                                <tr className="en-tête_tableau">
+                                    <th className="tt_nom_societe">Nom / Société</th>
+                                    <th className="tt_email">Email</th>
+                                    <th className="tt_tel">Téléphone</th>
+                                    <th className="tt_facture">Facture en cours</th>
+                                    <th className="tt_solde">Solde dû</th>
+                                    <th className="tt_derniere_activite">Dernière activité</th>
+                                </tr>
+                            </thead>
+
+                            {/* for each clients */}
+                            <tbody>
+                                <tr className="tableau_clients">
+                                    <td className="nom_societe">Bernard Guy</td>
+                                    <td className="email">bernardguy@gmail.com</td>
+                                    <td className="tel">06 67 78 89 90</td>
+                                    <td className="facture">5318-90</td>
+                                    <td className="solde">1 150 €</td>
+                                    <td className="derniere_activite">07/12/25</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
 
                     {/* Section de la duexième année */}
                     <div className="section-annee">
                         <h2 className="titre-annee">2025</h2>
-                        {/* Liste des documents ici */}
-                        <p>Devis 1</p>
-                        <p>Devis 2</p>
-                        <p>Devis 3</p>
-                        <p>Devis 4</p>
-                        <p>Devis 5</p>
-                        <p>Devis 6</p>
-                        <p>Devis 7</p>
+                        <table className="tableau_clients">
+                            <thead>
+                                <tr className="en-tête_tableau">
+                                    <th className="tt_nom_societe">Nom / Société</th>
+                                    <th className="tt_email">Email</th>
+                                    <th className="tt_tel">Téléphone</th>
+                                    <th className="tt_facture">Facture en cours</th>
+                                    <th className="tt_solde">Solde dû</th>
+                                    <th className="tt_derniere_activite">Dernière activité</th>
+                                </tr>
+                            </thead>
+
+                            {/* for each clients */}
+                            <tbody>
+                                <tr className="tableau_clients">
+                                    <td className="nom_societe">Bernard Guy</td>
+                                    <td className="email">bernardguy@gmail.com</td>
+                                    <td className="tel">06 67 78 89 90</td>
+                                    <td className="facture">5318-90</td>
+                                    <td className="solde">1 150 €</td>
+                                    <td className="derniere_activite">07/12/25</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                     </div>
                 </div>
             </div>
