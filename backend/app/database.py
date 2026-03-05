749e0d345de9e17afcb3482b97694fe8bd68f06b
ManonMaMa <infini030303@gmail.com>
2026-03-05 13:57:23 +0100

    environnement ok

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import Session
 
-DATABASE_URL = "postgresql://postgres:handigital@localhost:5432/appli_gestion_entreprise"
+DATABASE_URL = "postgresql://postgres:handigital@db:5432/appli_gestion_entreprise"
 
 engine = create_engine(DATABASE_URL)
 
